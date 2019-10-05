09b05187b007fe5a790eac0d0f5b74e54cee852f
samuel.cabrera.1999@gmail.com <samuel.cabrera.1999@gmail.com>
2019-10-05 13:46:06 -0400

    hey falto conectar a un celular

---
+++ mbcPhone/AppPhone/AppPhone/Models/Phone.cs
@@ -0,0 +1,21 @@
+﻿namespace AppPhone.Models
+{
+    public enum TypeContact
+    {
+        PhoneNumber,
+        Email,
+        Faceboock,
+        Twitter,
+        LinkedIN
+    }
+
+
+    public class Phone
+    {
+        public int PhoneID { get; set; }
+        public string Name { get; set; }
+        public TypeContact Type { get; set; }
+        public string Contact { get; set; }
+
+    }
+}
