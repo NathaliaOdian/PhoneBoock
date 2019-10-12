e60061e1282cef4cb05fe5f9325678d44adc6f78
juampi630@gmail.com <juampi630@gmail.com>
2019-10-12 13:16:40 -0400

    MVVM completo

---
+++ mbcPhone/AppPhone/AppPhone/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace AppPhone.ViewModels
+{
+    using AppPhone.Models;
+    using System.Collections.Generic;
+
+    public class MainViewModel
+    {
+        #region Properties
+        public List<Phone> ListPhone { get; set; }
+        #endregion
+
+        #region ViewModels
+        public PhoneBookViewModel phoneBookViewModel { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            phoneBookViewModel = new PhoneBookViewModel();
+        } 
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MainViewModel();
+            }
+            return (instance);
+        }
+        #endregion
+
+    }
+}
